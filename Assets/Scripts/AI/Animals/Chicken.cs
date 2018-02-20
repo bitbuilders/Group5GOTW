268e014459a564290679f357e68dfe7a42295d90
DESKTOP-EH80S66\Caleb Carlile <caleb165@gmail.com>
2018-02-20 12:41:27 -0700

    Some code changes

---
+++ Assets/Scripts/AI/Animals/Chicken.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : Animal 
+{
+	#region Variables (private)
+	
+	#endregion
+	
+	#region Properties (public)
+	
+	#endregion
+	
+	#region Unity event functions
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+	
+	#endregion
+	
+	#region Methods
+	
+	#endregion
+}
