058267934fe42266406eddb009a7536216203fdb
Colin Misbach <CMisbach@student.neumont.edu>
2018-02-22 16:52:57 -0700

    Worked on level design in my scene

--- Assets/Scripts/Crop.cs
+++ Assets/Scripts/Crop.cs
@@ -7,12 +7,12 @@ public class Crop : MonoBehaviour
 {
     [SerializeField] [Range(0.1f, 20.0f)] float m_maxLife = 3.0f;
     [SerializeField] [Range(0.1f, 20.0f)] float m_respawnTime = 3.0f;
-    [SerializeField] KeyCode m_eatKey = KeyCode.Space;
     [SerializeField] GameObject m_cropHUD;
 
     ParticleSystem m_particleSystem;
     SphereCollider m_collider;
     MeshRenderer m_meshRenderer;
+    TextMeshProUGUI m_hudText;
     float m_life;
     float m_time;
 
@@ -23,6 +23,7 @@ public class Crop : MonoBehaviour
         m_collider = GetComponent<SphereCollider>();
         m_meshRenderer = GetComponentInChildren<MeshRenderer>();
         m_particleSystem = GetComponentInChildren<ParticleSystem>();
+        m_hudText = m_cropHUD.GetComponentInChildren<TextMeshProUGUI>();
 
         m_life = m_maxLife;
         m_time = 0.0f;
@@ -54,6 +55,12 @@ public class Crop : MonoBehaviour
                 m_life = m_maxLife;
             }
             m_cropHUD.SetActive(true);
+
+            Vector3 direction = other.transform.position - transform.position;
+            direction.x = 0.0f;
+            direction.z = 0.0f;
+            m_hudText.transform.LookAt(other.transform.position + Vector3.up * 3 - direction);
+            m_hudText.transform.Rotate(0.0f, 180.0f, 0.0f);
         }
 
         if (Eaten)
